3d6c7e9aa6064ba70961668dc45b21dba4df67c8
kevshah14 <kevshah14@gmail.com>
2015-10-31 16:48:30 -0400

    Refactored file names to eliminate errors

---
+++ YourTurnMobileApp/app/src/main/java/com/example/yourturnmobileapp/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package com.example.yourturnmobileapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Keval on 31-10-2015.
+ */
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
