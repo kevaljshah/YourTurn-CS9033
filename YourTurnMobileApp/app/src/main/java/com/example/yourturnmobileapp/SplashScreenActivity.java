72a54a1a00c9add3c9ea5de15bc197316630d488
kevshah14 <kevshah14@gmail.com>
2015-10-31 19:07:29 -0400

    SplashScreen code completed

--- YourTurnMobileApp/app/src/main/java/com/example/yourturnmobileapp/SplashScreenActivity.java
+++ YourTurnMobileApp/app/src/main/java/com/example/yourturnmobileapp/SplashScreenActivity.java
@@ -1,8 +1,12 @@
 package com.example.yourturnmobileapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import com.example.yourturnmobileapp.R;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import com.example.yourturnmobileapp.MainActivity;
 
 /**
  * Created by Keval on 31-10-2015.
@@ -13,5 +17,31 @@ public class SplashScreenActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        final ImageView slr = (ImageView) findViewById(R.id.imageView);
+        final Animation rt = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
+        //final Animation fo = AnimationUtils.loadAnimation(getBaseContext(),R.anim.fade_out);
+
+        slr.startAnimation(rt);
+        rt.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                finish();
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 }
