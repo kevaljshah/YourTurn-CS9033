6959e634cec2247a6cc19c050dc61565274676f1
kevshah14 <kevshah14@gmail.com>
2015-10-31 17:03:40 -0400

    SplashScreen Base commit

--- YourTurnMobileApp/app/src/main/java/com/example/yourturnmobileapp/SplashScreenActivity.java
+++ YourTurnMobileApp/app/src/main/java/com/example/yourturnmobileapp/SplashScreenActivity.java
@@ -11,5 +11,6 @@ public class SplashScreenActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activiy_splashscreen);
     }
 }
