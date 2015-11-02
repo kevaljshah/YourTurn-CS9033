4cf34956c46f4113270e3325c7c01d15518ae1b5
ApoorvaW <apoorva.walimbe@gmail.com>
2015-11-02 14:03:46 -0500

    Add task activity & view

--- YourTurnMobileApp/app/src/main/java/com/example/yourturnmobileapp/AddTaskActivity.java
+++ YourTurnMobileApp/app/src/main/java/com/example/yourturnmobileapp/AddTaskActivity.java
@@ -2,16 +2,62 @@ package com.example.yourturnmobileapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 import com.example.yourturnmobileapp.R;
+
+import java.security.PublicKey;
+
+import models.Task;
 
 /**
  * Created by Apoorva Walimbe on 10/31/2015.
  */
 public class AddTaskActivity extends Activity {
 
+    private static final String TAG = "AddTaskActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_addtask);
+
+        final ImageButton createTask = (ImageButton) findViewById(R.id.submitTask);
+        createTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Task task = createTask();
+                saveTask(task);
+            }
+        });
+
+        final ImageButton cancelTask = (ImageButton) findViewById(R.id.cancelTask);
+        cancelTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancelTask();
+            }
+        });
     }
+
+    public Task createTask() {
+        Task task = new Task();
+        task.setTaskName(findViewById(R.id.taskName).toString());
+        task.setTaskDesc(findViewById(R.id.taskDesc).toString());
+        task.setAssignee(findViewById(R.id.assignee).toString());
+        task.setDueDate(findViewById(R.id.dueDate).toString());
+        return task;
+    }
+
+    public void saveTask(Task task) {
+        //code to persist the data into database
+    }
+
+    public void cancelTask() {
+
+    }
+
+
 }
