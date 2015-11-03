9ac3916af3aa7b109249b722d2ca876e9bb75925
kevshah14 <kevshah14@gmail.com>
2015-11-02 19:16:05 -0500

    updated Main Activity and removed previous crash errors

--- YourTurnMobileApp/app/src/main/java/com/example/yourturnmobileapp/MainActivity.java
+++ YourTurnMobileApp/app/src/main/java/com/example/yourturnmobileapp/MainActivity.java
@@ -4,16 +4,32 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.yourturnmobileapp.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+	Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+		button = (Button) findViewById(R.id.button);
+		button.setOnClickListener(this);
     
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.button:
+				startActivity((new Intent(this,AddTaskActivity.class)));
+
+				break;
+		}
 	}
 	public void startCreateSupplyList(View view)
 	{
