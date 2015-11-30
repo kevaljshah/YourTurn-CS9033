2832e23520728224a1c1862847ca09c52bcd2707
kevshah14 <kevshah14@gmail.com>
2015-11-30 14:04:57 -0500

    Facebook Login complete and working

--- YourTurnMobileApp/app/src/main/java/com/example/yourturnmobileapp/LoginActivity.java
+++ YourTurnMobileApp/app/src/main/java/com/example/yourturnmobileapp/LoginActivity.java
@@ -9,6 +9,13 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 /**
  * Created by Keval on 02-11-2015.
  */
@@ -16,10 +23,17 @@ public class LoginActivity extends Activity implements View.OnClickListener {
     ImageButton imageButton;
     EditText editText,editText2;
     Button Reghere;
+    LoginButton fblogin;
+    Intent data;
+    private CallbackManager callbackManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        callbackManager= CallbackManager.Factory.create();
         setContentView(R.layout.activity_login);
 
         editText = (EditText) findViewById(R.id.editText);
@@ -28,10 +42,41 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         Reghere = (Button) findViewById(R.id.Reghere);
         imageButton.setOnClickListener(this);
         Reghere.setOnClickListener(this);
+        fblogin = (LoginButton) findViewById(R.id.fblogin_button);
+
+        fblogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                startActivity(data);
+
+            }
+
+            @Override
+            public void onCancel() {
+
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
 
 
     }
 
+    public void startMainActivity(View view) {
+
+        // TODO - fill in here
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     public void onClick(View v)
